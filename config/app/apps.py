d54fd50a5bea2df1431127a9561b5004f2500599
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-05-26 19:14:04 +0900

    개발 이전 작업

--- config/app/apps.py
+++ config/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BookmarkConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookmark'
+    name = 'app'

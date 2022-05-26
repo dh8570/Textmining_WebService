2c9a2cbba43946bc03f4ecdf0793be8cfe9b680e
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-05-26 21:25:32 +0900

    개발 이전 작업

--- config/app/models.py
+++ config/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+    text = models.CharField(max_length=1000)
+    label = models.CharField(max_length=10)
+    tags = models.CharField(max_length=100)
--- config/app/views.py
+++ config/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Document
 
 # Create your views here.
+class AppListView(ListView):
+    # 어떤 모델(테이블)에 대한 제너릭 뷰인지 지정
+    model = Document
+
+class AppCreateView(CreateView):
+    model = Document
+    fields = ['text']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'

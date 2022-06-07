ae0f64237af01b9117e1323a9f155d17fdbfcc80
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-06-07 18:59:19 +0900

    프로그래스바 백엔드 연결 및 새로 고침 버그 사항 수정

--- config/app/python/list.py
+++ config/app/python/list.py
@@ -1,4 +1,5 @@
 import sqlite3
+from .parse import class_dic
 from django.shortcuts import render
 
 
@@ -13,6 +14,44 @@ def search_data():
     return rows
 
 
+def progress_data():
+    conn = sqlite3.connect('./db.sqlite3', isolation_level=None)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT category FROM APP_DOCUMENT')
+    rows = cursor.fetchall()
+    conn.close()
+
+    category_amount = {}
+    for i, row in enumerate(rows):
+        if row[0] not in list(class_dic.values()):
+            if '기타' not in list(category_amount.keys()):
+                category_amount['기타'] = 1
+            else:
+                category_amount['기타'] += 1
+        rows[i] = row[0]
+
+    for category_value in class_dic.values():
+        if rows.count(category_value) != 0:
+            category_amount[category_value] = rows.count(category_value)
+
+    progressbar_data = []
+    total_amount = len(rows)
+    for category, amount in category_amount.items():
+        percent = (amount / total_amount) * 100
+        data = (category, percent)
+        progressbar_data.append(data)
+
+    progressbar_data.sort(key=lambda x: -x[1])
+    progressbar_key = []
+    progressbar_value = []
+    for data in progressbar_data:
+        progressbar_key.append(data[0])
+        progressbar_value.append(data[1])
+
+    return progressbar_key, progressbar_value
+
+
 def post_doc(request):
     title_list = search_data()
     context = {}
@@ -20,5 +59,11 @@ def post_doc(request):
     list_len = int(len(title_list)) + 1
     for i in range(1, list_len):
         context['list' + str(i)] = title_list[i-1]
+
+    key, value = progress_data()
+    category_len = int(len(key))
+    context['progressbar_key'] = key
+    context['progressbar_value'] = value
+    context['progressbar_len'] = category_len
 
     return render(request, './app/document_list.html', context)
